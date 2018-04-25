3e0910b772277ffb899588e9af778e28f10fa6d7
Kostt <felix-mazur@mail.ru>
2018-04-25 12:48:52 +0300

    u

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -11,11 +11,11 @@ var mainView = myApp.addView('.view-main', {
 
 });
 
-if(window.localStorage.getItem('has_run') == null) {
+// if(window.localStorage.getItem('has_run') == null) {
     // myApp.popup('.start-popup');
     // window.localStorage.setItem('has_run', 'true');
     mainView.router.loadPage('auth.html');
-}
+// }
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
