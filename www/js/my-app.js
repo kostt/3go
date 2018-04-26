1d05b102860d8836705609f4d0638155b75e4c90
Kostt <felix-mazur@mail.ru>
2018-04-26 14:04:37 +0300

    bug

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -695,106 +695,106 @@ function statistics(stat_time){
 
 myApp.onPageInit('auth', function (page) {
 
-    // FirebaseUI config.
-    var uiConfig = {
-        callbacks: {
-            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                var user = authResult.user;
-                var credential = authResult.credential;
-                var isNewUser = authResult.additionalUserInfo.isNewUser;
-                var providerId = authResult.additionalUserInfo.providerId;
-                var operationType = authResult.operationType;
-                // Do something with the returned AuthResult.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
-                return true;
-            },
-            signInSuccess: function(currentUser, credential, redirectUrl) {
-
-                alert("Да");
-                // This callback will be deprecated. `signInSuccessWithAuthResult` should
-                // be used instead.
-                // Do something.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
-                return true;
-            },
-            signInFailure: function(error) {
-
-                alert("Да");
-                // Some unrecoverable error occurred during sign-in.
-                // Return a promise when error handling is completed and FirebaseUI
-                // will reset, clearing any UI. This commonly occurs for error code
-                // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-                // occurs. Check below for more details on this.
-                return handleUIError(error);
-            },
-            uiShown: function() {
-                // The widget is rendered.
-                // Hide the loader.
-                document.getElementById('loader').style.display = 'none';
-            }
-        },
-        credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
-        // Query parameter name for mode.
-        queryParameterForWidgetMode: 'mode',
-        // Query parameter name for sign in success url.
-        queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: 'index.html',
-        signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            {
-                provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                // Whether the display name should be displayed in the Sign Up page.
-                requireDisplayName: true
-            },
-            {
-                // provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-                // Invisible reCAPTCHA with image challenge and bottom left badge.
-                recaptchaParameters: {
-                    type: 'image',
-                    size: 'invisible',
-                    badge: 'bottomleft'
-                }
-            }
-        ],
-        // Terms of service url.
-        tosUrl: '<your-tos-url>'
-    };
-
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
-
-
-
-
-    //
     // // FirebaseUI config.
     // var uiConfig = {
+    //     callbacks: {
+    //         signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //             var user = authResult.user;
+    //             var credential = authResult.credential;
+    //             var isNewUser = authResult.additionalUserInfo.isNewUser;
+    //             var providerId = authResult.additionalUserInfo.providerId;
+    //             var operationType = authResult.operationType;
+    //             // Do something with the returned AuthResult.
+    //             // Return type determines whether we continue the redirect automatically
+    //             // or whether we leave that to developer to handle.
+    //             return true;
+    //         },
+    //         signInSuccess: function(currentUser, credential, redirectUrl) {
+    //
+    //             alert("Да");
+    //             // This callback will be deprecated. `signInSuccessWithAuthResult` should
+    //             // be used instead.
+    //             // Do something.
+    //             // Return type determines whether we continue the redirect automatically
+    //             // or whether we leave that to developer to handle.
+    //             return true;
+    //         },
+    //         signInFailure: function(error) {
+    //
+    //             alert("Да");
+    //             // Some unrecoverable error occurred during sign-in.
+    //             // Return a promise when error handling is completed and FirebaseUI
+    //             // will reset, clearing any UI. This commonly occurs for error code
+    //             // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+    //             // occurs. Check below for more details on this.
+    //             return handleUIError(error);
+    //         },
+    //         uiShown: function() {
+    //             // The widget is rendered.
+    //             // Hide the loader.
+    //             document.getElementById('loader').style.display = 'none';
+    //         }
+    //     },
+    //     credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+    //     // Query parameter name for mode.
+    //     queryParameterForWidgetMode: 'mode',
+    //     // Query parameter name for sign in success url.
+    //     queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
+    //     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    //     signInFlow: 'popup',
     //     signInSuccessUrl: 'index.html',
     //     signInOptions: [
     //         // Leave the lines as is for the providers you want to offer your users.
-    //         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     //         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //         firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    //         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //         firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    //         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //         {
+    //             provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //             // Whether the display name should be displayed in the Sign Up page.
+    //             requireDisplayName: true
+    //         },
+    //         {
+    //             // provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    //             // Invisible reCAPTCHA with image challenge and bottom left badge.
+    //             recaptchaParameters: {
+    //                 type: 'image',
+    //                 size: 'invisible',
+    //                 badge: 'bottomleft'
+    //             }
+    //         }
     //     ],
     //     // Terms of service url.
     //     tosUrl: '<your-tos-url>'
     // };
     //
-    // // Initialize the FirebaseUI Widget using Firebase.
     // var ui = new firebaseui.auth.AuthUI(firebase.auth());
     // // The start method will wait until the DOM is loaded.
     // ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+
+
+    // FirebaseUI config.
+    var uiConfig = {
+        signInSuccessUrl: 'index.html',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
 
 
 
