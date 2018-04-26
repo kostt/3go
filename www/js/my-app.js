0ede18fc068e6763d03cc5d0c942611bea181ea9
Kostt <felix-mazur@mail.ru>
2018-04-26 17:17:32 +0300

    bug

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -224,23 +224,23 @@ myApp.onPageInit('wizard', function (page) {
     $$('.number').on("keydown",function(event){
         if (event.keyCode == 190) {
             event.preventDefault();
-            $$(this).val($$(this).val());
-            alert(event.keyCode);
+            var str = $$(this).val();
+            str.substring(0, str.length - 1)
         }
         if (event.keyCode == 188) {
             event.preventDefault();
-            $$(this).val($$(this).val());
-            alert(event.keyCode);
+            var str = $$(this).val();
+            str.substring(0, str.length - 1)
         }
         if (event.keyCode == 189) {
             event.preventDefault();
-            $$(this).val($$(this).val());
-            alert(event.keyCode);
+            var str = $$(this).val();
+            str.substring(0, str.length - 1)
         }
         if (event.keyCode == 229) {
             event.preventDefault();
-            $$(this).val($$(this).val());
-            alert(event.keyCode);
+            var str = $$(this).val();
+            str.substring(0, str.length - 1)
         }
     });
 
